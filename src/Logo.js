c49888040e5fb55b63b994177e86855ee21c7849
Maigo Erit <maigo.erit@gmail.com>
2021-07-16 20:40:40 +0300

    replace old homepage

---
+++ src/Logo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Image, keyframes, usePrefersReducedMotion } from '@chakra-ui/react';
+import logo from './logo.jpg';
+
+const spin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`;
+
+export const Logo = props => {
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  const animation = prefersReducedMotion
+    ? undefined
+    : `${spin} infinite 20s linear`;
+
+  return (
+    <Image
+      borderRadius="full"
+      width="150px"
+      height="150px"
+      animation={animation}
+      src={logo}
+      {...props}
+    />
+  );
+};
